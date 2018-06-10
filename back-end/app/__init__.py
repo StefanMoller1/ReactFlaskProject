6e9a78982fa3f2a4aee52efd455dbc1c04e51166
Stefan <stefan.moller@eighty20.co.za>
2018-06-10 14:59:50 +0200

    build framework

---
+++ back-end/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from .worker import api_prepair, logging
+
+app = Flask(__name__)
+CORS(app)
+
+##########################################################
+## NAME:		CATCH_ALL
+## PARAMS: 		(PATH), 1
+## AUTHOR: 		STEFAN
+## DESCRIPTION:	CATCH ALL PARAMS AND SEND IT THROU TO THE
+##				METHODS FILE
+##########################################################
+@app.route('/<path:path>',  methods=["GET","POST"])
+def catch_api(path):
+	data = request.data.decode('ascii')
+	service = api_prepair(path, data)
+	return jsonify(service), 200
+
+if __name__ is "__main__":
+	app.run()
